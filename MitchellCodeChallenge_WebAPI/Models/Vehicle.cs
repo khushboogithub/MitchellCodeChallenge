c7f07ff3418875a28d4378156fab9daeb3bcd01b
Khushboo Gupta <Khushboo Gupta@LAPTOP-USSQ5OB2>
2021-10-01 08:29:03 -0700

    change

---
+++ MitchellCodeChallenge_WebAPI/Models/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MitchellCodeChallenge_WebAPI.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(4)]
+        public int Year { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Make { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Model { get; set; }
+    }
+}
